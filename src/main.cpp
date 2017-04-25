ee088a3eeed6a8b46cdaa7d5aee321a695f19d0b
penguin1214 <jinglei_yang@outlook.com>
2017-04-25 15:57:12 +0800

    Create empty window using GLFW instead to handle mouse events more easily.

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,38 @@
 //
 
 #include <iostream>
-#include "window.h"
+#include <GLFW/glfw3.h>
 
 const int width = 800;
 const int height = 800;
 
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
 int main() {
-    Window win(width, height, "Rendr");
-    win.show();
+    GLFWwindow* window;
+
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) exit(EXIT_FAILURE);
+
+    window = glfwCreateWindow(width, height, "Rendr", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
